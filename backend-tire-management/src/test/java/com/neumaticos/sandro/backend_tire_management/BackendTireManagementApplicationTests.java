86add2cd113a0394cf4d808ecf998747b27330da
Barex8 <alebalsas8@gmail.com>
2026-01-21 17:03:41 +0100

    backend-init

---
+++ backend-tire-management/src/test/java/com/neumaticos/sandro/backend_tire_management/BackendTireManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neumaticos.sandro.backend_tire_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTireManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
